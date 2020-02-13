1a6ca58a0ed0a77291eef6a6e566a6984fed069c
援军 <34418258+menhood@users.noreply.github.com>
2020-02-13 13:21:50 +0800

    排版

--- 404.php
+++ 404.php
@@ -7,8 +7,15 @@
             <h2 class="post-title">404 - <?php _e('页面没找到'); ?></h2>
             <p><?php _e('你想查看的页面已被转移或删除了, 要不要搜索看看: '); ?></p>
             <form method="post">
-                <p><input type="text" name="s" class="text" autofocus /></p>
-                <p><button type="submit" class="submit"><?php _e('搜索'); ?></button></p>
+                <p><input type="text" name="s" class="text" autofocus />
+                <button type="submit" class="submit btn"><?php _e('搜索'); ?></button></p>
+                <!--标签云-->
+                <?php $this->widget('Widget_Metas_Tag_Cloud', 'ignoreZeroCount=1&limit=30')->to($tags); ?>
+                <div class="tags-list">
+                <?php while($tags->next()): ?>
+                    <a href="<?php $tags->permalink(); ?>" title='<?php $tags->name(); ?>'><?php $tags->name(); ?></a>
+                <?php endwhile; ?>
+                </div>  
             </form>
         </div>
 
